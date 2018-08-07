30d9a5ee165b15f3e1a9f0bdd8bfa35135ad6f6f
Johnny Soong <johnnysoong@hotmail.com>
2018-08-07 00:53:00 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,22 @@
+def sort_array_asc(array) 
+  array.sort do |a, b|
+    a <=> b
+  end
+end
+
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.collect do |element|
+    element.length
+  end.sort do |a, b|
+    a <=> b
+  end
+end
+  
+
 
