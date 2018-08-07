730c49f2eaccf843f5650cd832328839ce435f78
Johnny Soong <johnnysoong@hotmail.com>
2018-08-07 04:24:00 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -31,13 +31,9 @@ end
 
 def kesha_maker(array)
   arr = []
-  i = 0
-  while i < array.length
-  arr_char = array[i].split("")
-  arr_char[2] = "$"
-  new_word = arr_char.join("")
-  arr << new_word
-  i += 1
+  array.each do |word|
+    word[2] = "$"
+    arr << word 
   end
   arr
 end
@@ -45,26 +41,22 @@ end
 
 def find_a(array)
    array.select do |word|
-   word[0] == "a"
+     word.start_with?("a")
    end
 end
 
 def sum_array(array)
-  sum = 0
-  array.each do |num|
-    sum += num
-  end 
-  sum
+  array.inject do |sum, value|
+    sum += value
+  end
 end
 
 def add_s(array)
-  arr = []
-  array.each_with_index do |word, i|
-    if i != 1
-    arr << array[i] + 's'
-    else 
-    arr << array[i] 
+  array.collect.each_with_index do |element, index|
+  if index != 1
+    element + "s"
+  else
+    element
   end
-  end
-  arr
+ end
 end
