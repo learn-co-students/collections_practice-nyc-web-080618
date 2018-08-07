a5a6e93edf073cfda093bd7c6d431a291d7b729f
Johnny Soong <johnnysoong@hotmail.com>
2018-08-07 01:44:05 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,12 +11,58 @@ def sort_array_desc(array)
 end
 
 def sort_array_char_count(array)
-  array.collect do |element|
-    element.length
-  end.sort do |a, b|
-    a <=> b
-  end
+ array.sort do |a, b|
+  a.length <=> b.length
+ end
 end
   
 
+def swap_elements(array)
+  temp = array[1]
+  array[1] = array[2]
+  array[2] = temp
+  array
+end
 
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  arr = []
+  i = 0
+  while i < array.length
+  arr_char = array[i].split("")
+  arr_char[2] = "$"
+  new_word = arr_char.join("")
+  arr << new_word
+  i += 1
+  end
+  arr
+end
+
+def find_a(array)
+   array.select do |word|
+   word[0] == "a"
+   end
+end
+
+def sum_array(array)
+  sum = 0
+  array.each do |num|
+    sum += num
+  end 
+  sum
+end
+
+def add_s(array)
+  arr = []
+  array.each_with_index do |word, i|
+    if i != 1
+    arr << array[i] + 's'
+    else 
+    arr << array[i] 
+  end
+  end
+  arr
+end
